badb73dff06d338df2060308bb541c5a8746eefa
vigneshpyc <145823761+vigneshpyc@users.noreply.github.com>
2025-11-08 19:56:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import json
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,  # Allow cookies and authorization headers
+    allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],     # Allow all headers in the request
+)
+
+@app.get('/')
+async def home():
+    return "this is home page"
+
+@app.post('/username')
+async def username(request : Request):
+    data = await request.json()
+    print("Received:", data)
+    return {"username": data["username"]}
