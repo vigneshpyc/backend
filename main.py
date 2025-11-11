118b604f2ee1f53cf577b72af11b6106a00ae9bb
vigneshpyc <jeevaviki1609@gmail.com>
2025-11-11 22:33:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,6 @@ app.add_middleware(
     allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],     # Allow all headers in the request
 )
-from fastapi.responses import JSONResponse
-
-@app.options("/{rest_of_path:path}")
-async def preflight_handler(rest_of_path: str):
-    return JSONResponse(
-        status_code=200,
-        content={"message": "Preflight OK"},
-        headers={
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers": "*",
-        }
-    )
 
 @app.get('/')
 async def home():
