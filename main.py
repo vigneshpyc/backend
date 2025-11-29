5d3ddd4648a11668e588fb210704723868aa2f5b
vigneshpyc <jeevaviki1609@gmail.com>
2025-11-29 19:47:17 +0530

    Written a more API

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pymongo import MongoClient
 import json
 app = FastAPI()
 app.add_middleware(
@@ -20,3 +22,71 @@ async def username(request : Request):
     data = await request.json()
     print("Received:", data)
     return {"username": data["username"]}
+#API for Time table data insertion
+@app.post("/TT_Data")
+async def timeTableData(request:Request):
+    if request.method == "POST":
+        try:
+            data = await request.json()
+            client = MongoClient("mongodb://127.0.0.1:27017/")
+            db = client['TTcheck']
+            collection = db['TTData']
+            print("-----------------DataBase Connected successfully-------------------")
+        except Exception as e:
+            print("----------------DataBase Not Connected, check Connection-------------------",e)
+        try:
+            collection.insert_one({"data":data})
+            print("----------Data inserted Successfully---------")
+            return JSONResponse("Data Inserted Successfully")
+        except Exception as e:
+            return {"Something went wrong",e}
+    else:
+        return "Method not Allowed"
+
+#API for insert class data (eg:year, section)
+@app.post('/class_data')
+async def class_data(request:Request):
+    if request.method == "POST":
+        try:
+            data = await request.json()
+            client = MongoClient("mongodb://127.0.0.1:27017/")
+            db = client['TTcheck']
+            collection = db['Class_datas']
+           
+        except Exception as e:
+            return JSONResponse({"message":"Database not Connected"})
+        #Inert a Data into DB
+        try:
+            for item in data:
+                collection.insert_one(item)
+            
+            return JSONResponse("Data Inserted Successfully")
+        except Exception as e:
+            return {"Something went wrong",e}
+    else:
+        return "Method not Allowed"
+
+#API for get the class_data from db
+@app.post("/get_class_data")
+async def get_class_data(request:Request):
+    if request.method == "POST":
+        try:
+            data = await request.json()
+            year = data.get("year")
+            section = data.get("section")
+
+            #connection DB
+            client = MongoClient("mongodb://127.0.0.1:27017/")
+            db = client['TTcheck']
+            collection = db['Class_datas']
+
+            #data fetching
+            datas = collection.find_one({"year":year,"section":section})
+            if not datas:
+                return {"message":"Invalid Credentials"}
+            datas["_id"] = str(datas["_id"])
+            return datas
+        except Exception as e:
+            return "Unable to retrieve a data"
+    else:
+        return "Method not allowed"
